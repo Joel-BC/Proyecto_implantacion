e767c378db8671bf6487218878ea19aaf4f0f2ef
Usuario <Usuario@DESKTOP-GE5HRHG>
2020-09-19 13:33:06 -0800

    Agregar archivos de proyecto.

---
+++ Consola/Controllers/SancionController.cs
@@ -0,0 +1,177 @@
+﻿using BLL;
+using Consola.Helpers;
+using Consola.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Consola.Controllers
+{
+    [SessionManage]
+    public class SancionController : Controller
+    {
+        // GET: Sancion
+        public ActionResult Index()
+        {
+            try
+            {
+                List<Sancion> listaSancion = new List<Sancion>();
+                clsSancion activo = new clsSancion();
+                var data = activo.ConsultarSancion().ToList();
+
+                foreach (var item in data)
+                {
+                    Sancion modelo = new Sancion();
+                    modelo.idSanciones = item.idSanciones;
+                    modelo.idEmpleado = item.IdEmpleado;
+                    modelo.idDepartamento = item.idDepartamento;
+                    modelo.fechaSancion = (DateTime)item.fechaSancion;
+                    modelo.codigo = item.codigo;
+                    modelo.nombre = item.nombre;
+                    modelo.detalle = item.detalle;
+                    modelo.estadoSancion = item.estadoSancion;
+
+                    listaSancion.Add(modelo);
+                }
+                return View(listaSancion);
+            }
+            catch
+            {
+                string descMsg = "Error consultando la informacion del CLiente.";
+                //Bitacora
+                return RedirectToAction("Error", "Error");
+            }
+        }
+
+        // GET: Sancion/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Crear()
+        {
+            clsSancion ObjSancion = new clsSancion();
+            ViewBag.Lista = ObjSancion.ConsultarDepartamento().ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Crear(Sancion sancion, string txtDetalleSancion, int listIdDepartamento)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    clsSancion ObjSancion = new clsSancion();
+                    clsUsuario objUsuario = new clsUsuario();
+                    clsBitacoraSancion objBitacoraSancion = new clsBitacoraSancion();
+
+                    bool Resultado = ObjSancion.AgregarSancion(sancion.idEmpleado, listIdDepartamento, sancion.fechaSancion, sancion.codigo,
+                        sancion.nombre.ToString(), txtDetalleSancion, true);
+
+                    string nombreUsuario = (string)Session["Usuario"];
+                    int IdUsuario = objUsuario.ConsultarIdUsuario(nombreUsuario);
+
+                    objBitacoraSancion.AgregarBitacoraSancion(IdUsuario, nombreUsuario, DateTime.Now, sancion.idEmpleado, listIdDepartamento, sancion.fechaSancion, sancion.codigo,
+                        sancion.nombre, txtDetalleSancion, true);
+
+                    if (Resultado)
+                    {
+                        TempData["exitoMensaje"] = "La sanción se ha insertado exitosamente.";
+                        return RedirectToAction("Crear");
+                    }
+                    else
+                    {
+                        TempData["errorMensaje"] = "Se presentó un error al intentar insertar este elemento, revise que los datos coincidan con lo que especifican los campos";
+                        return View("Crear");
+                    }
+                }
+                else
+                {
+                    return View("Crear");
+                }
+            }
+            catch
+            {
+                TempData["errorMensaje"] = "Todos los campos son obligatorios.";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            try
+            {
+                clsSancion sancion = new clsSancion();
+
+                ViewBag.Lista = sancion.ConsultarDepartamento().ToList();
+
+                var dato = sancion.ConsultaSancion(id);
+
+                Sancion modelo = new Sancion();
+                modelo.idSanciones = dato[0].idSanciones;
+                modelo.idEmpleado = dato[0].IdEmpleado;
+                modelo.idDepartamento = dato[0].idDepartamento;
+                modelo.fechaSancion = (DateTime)dato[0].fechaSancion;
+                modelo.codigo = dato[0].codigo;
+                modelo.nombre = dato[0].nombre;
+                modelo.detalle = dato[0].detalle;
+                modelo.estadoSancion = dato[0].estadoSancion;
+                return View(modelo);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // POST: Sancion/Edit/5
+        [HttpPost]
+        public ActionResult Editar(int id, Sancion sancion, int listIdDepartamento)
+        {
+            try
+            {
+                clsSancion Objsancion = new clsSancion();
+                clsUsuario objUsuario = new clsUsuario();
+                clsBitacoraSancion objBitacoraSancion = new clsBitacoraSancion();
+
+                ViewBag.Lista = Objsancion.ConsultarDepartamento().ToList();
+
+                bool Resultado = Objsancion.ActualizarSancion(sancion.idSanciones, sancion.idEmpleado, listIdDepartamento, sancion.fechaSancion, sancion.codigo,
+                        sancion.nombre, sancion.detalle, true);
+
+
+                string nombreUsuario = (string)Session["Usuario"];
+                int IdUsuario = objUsuario.ConsultarIdUsuario(nombreUsuario);
+
+                objBitacoraSancion.ActualizarBitacoraSancion(sancion.idSanciones, IdUsuario, nombreUsuario, DateTime.Now, sancion.idEmpleado, listIdDepartamento, sancion.fechaSancion, sancion.codigo,
+                        sancion.nombre, sancion.detalle, true);
+
+                TempData["exitoMensaje"] = "Los datos de la sanción se han modificado exitosamente.";
+                return View("Editar");
+            }
+            catch
+            {
+                TempData["errorMensaje"] = "Se produjo un error, revise los datos insertados.";
+                return View("Editar");
+            }
+        }
+
+        // GET: Proveedor/Deshabilitar/5
+        public ActionResult Deshabilitar(int id)
+        {
+            if (Session["ROLES"].Equals("Admin"))
+            {
+                clsSancion ObjSancion = new clsSancion();
+                ObjSancion.DeshabilitarSancion(id);
+                return RedirectToAction("Index");
+            }
+            TempData["alertaMensaje"] = "El usuario con el que ha ingresado no tiene permiso para realizar esta operación.";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Consola/Controllers/StockController.cs
@@ -0,0 +1,186 @@
+﻿using BLL;
+using Consola.Helpers;
+using Consola.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Consola.Controllers
+{
+    [SessionManage]
+    public class StockController : Controller
+    {
+        //// GET: Stock
+        //public ActionResult Index()
+        //{
+        //    try
+        //    {
+        //        List<Stock> listaStock = new List<Stock>();
+        //        clsStock stock = new clsStock();
+        //        var data = stock.ConsultarStock().ToList();
+
+        //        foreach (var item in data)
+        //        {
+        //            Stock modelo = new Stock();
+        //            modelo.idStock = item.idStock;
+        //            modelo.codigo = item.idBodega;
+        //            modelo.nombreProducto = item.nombreProducto;
+        //            modelo.unidad = item.unidad;
+        //            modelo.codigoBodega = item.codigoBodega;
+        //            modelo.idProveedor = item.idProveedor;
+        //            modelo.estadoStock = item.estadoStock;
+
+        //            listaStock.Add(modelo);
+        //        }
+        //        return View(listaStock);
+        //    }
+        //    catch
+        //    {
+        //        string descMsg = "Error consultando la informacion del CLiente.";
+        //        //Bitacora
+        //        return RedirectToAction("Error", "Error");
+        //    }
+        //}
+
+        //// GET: Stock/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: Proveedores/Create
+        //[HttpGet]
+        //public ActionResult Crear()
+        //{
+        //    clsStock ObjStock = new clsStock();
+        //    ViewBag.Lista = ObjStock.ConsultarNombreBodega().ToList();
+        //    ViewBag.Lista2 = ObjStock.ConsultarIdProveedor().ToList();
+        //    return View();
+        //}
+
+        //// GET: Stock/Create
+        //[HttpPost]
+        //public ActionResult Crear(Stock stocks, string txtCodigoBodega, int listIdProveedor)
+        //{
+        //    try
+        //    {
+        //        if (ModelState.IsValid)
+        //        {
+        //            clsStock objstock = new clsStock();
+        //            clsUsuario objUsuario = new clsUsuario();
+        //            clsBitacoraStock objBitacoraStock = new clsBitacoraStock();
+
+        //            int IdBodega = objstock.ConsultaIdBodega(txtCodigoBodega);
+
+        //            bool Resultado = objstock.AgregarStock(IdBodega, stocks.nombreProducto, stocks.unidad, txtCodigoBodega, listIdProveedor, true);
+
+        //            string nombreUsuario = (string)Session["Usuario"];
+        //            int IdUsuario = objUsuario.ConsultarIdUsuario(nombreUsuario);
+
+        //            objBitacoraStock.AgregarBitacoraStock(IdUsuario, nombreUsuario, DateTime.Now, IdBodega, stocks.nombreProducto, stocks.unidad, txtCodigoBodega, listIdProveedor, true);
+
+        //            if (Resultado)
+        //            {
+        //                TempData["exitoMensaje"] = "El ítem se ha insertado exitosamente.";
+        //                return RedirectToAction("Crear");
+        //            }
+        //            else
+        //            {
+        //                TempData["errorMensaje"] = "Se presentó un error al intentar insertar este elemento, revise que los datos coincidan con lo que especifican los campos";
+        //                return View("Crear");
+        //            }
+        //        }
+        //        else
+        //        {
+        //            return View("Crear");
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        clsStock ObjStock = new clsStock();
+        //        ViewBag.Lista = ObjStock.ConsultarNombreBodega().ToList();
+        //        ViewBag.Lista2 = ObjStock.ConsultarIdProveedor().ToList();
+        //        TempData["errorMensaje"] = "Todos los campos son obligatorios.";
+        //        return View();
+        //    }
+        //}
+
+        //[HttpGet]
+        //public ActionResult Editar(int id)
+        //{
+        //    try
+        //    {
+        //        clsStock ObjStock = new clsStock();
+
+        //        ViewBag.Lista = ObjStock.ConsultarNombreBodega().ToList();
+        //        ViewBag.Lista2 = ObjStock.ConsultarIdProveedor().ToList();
+
+        //        var dato = ObjStock.ConsultaStock(id);
+
+        //        Stock modelo = new Stock();
+        //        modelo.idStock = dato[0].idStock;
+        //        modelo.codigo = dato[0].idBodega;
+        //        modelo.nombreProducto = dato[0].nombreProducto;
+        //        modelo.unidad = dato[0].unidad;
+        //        modelo.codigoBodega = dato[0].codigoBodega;
+        //        modelo.idProveedor = dato[0].idProveedor;
+        //        modelo.estadoStock = dato[0].estadoStock;
+        //        return View(modelo);
+        //    }
+        //    catch (Exception)
+        //    {
+        //        throw;
+        //    }
+
+        //}
+
+        //// POST: Activos/Edit/5
+        //[HttpPost]
+        //public ActionResult Editar(int id, Stock stock, string txtCodigoBodega, int listIdProveedor)
+        //{
+        //    try
+        //    {
+        //        clsStock ObjStock = new clsStock();
+        //        clsUsuario objUsuario = new clsUsuario();
+        //        clsBitacoraStock objBitacoraStock = new clsBitacoraStock();
+
+        //        ViewBag.Lista = ObjStock.ConsultarNombreBodega().ToList();
+        //        ViewBag.Lista2 = ObjStock.ConsultarIdProveedor().ToList();
+
+        //        int IdBodega = ObjStock.ConsultaIdBodega(txtCodigoBodega);
+
+        //        bool Resultado = ObjStock.ActualizarStock(stock.idStock, IdBodega, stock.nombreProducto,
+        //            stock.unidad, txtCodigoBodega, listIdProveedor, true);
+
+        //        string nombreUsuario = (string)Session["Usuario"];
+        //        int IdUsuario = objUsuario.ConsultarIdUsuario(nombreUsuario);
+
+        //        objBitacoraStock.ActualizarBitacoraStock(stock.idStock, IdUsuario, nombreUsuario, DateTime.Now, IdBodega, stock.nombreProducto, stock.unidad, txtCodigoBodega, listIdProveedor, true);
+
+        //        return View();
+        //    }
+        //    catch
+        //    {
+        //        clsStock ObjStock = new clsStock();
+        //        ViewBag.Lista = ObjStock.ConsultarNombreBodega().ToList();
+        //        ViewBag.Lista2 = ObjStock.ConsultarIdProveedor().ToList();
+        //        TempData["errorMensaje"] = "Inserte correctamente el formato de los datos.";
+        //        return View();
+        //    }
+        //}
+
+        //// GET: Proveedor/Deshabilitar/5
+        //public ActionResult Deshabilitar(int id)
+        //{
+        //    if (Session["ROLES"].Equals("Admin"))
+        //    {
+        //        clsStock ObjStock = new clsStock();
+        //        ObjStock.DeshabilitarStock(id);
+        //        return RedirectToAction("Index");
+        //    }
+        //    TempData["alertaMensaje"] = "El usuario con el que ha ingresado no tiene permiso para realizar esta operación.";
+        //    return RedirectToAction("Index");
+        //}
+    }
+}
